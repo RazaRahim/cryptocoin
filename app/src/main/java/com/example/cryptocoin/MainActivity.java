94ec3f2fa58a20f24ec23bb3a6a084095eced4fe
razaraheem <raza7571@gmail.com>
2020-10-16 15:40:30 +0500

    raza

--- app/src/main/java/com/example/cryptocoin/MainActivity.java
+++ app/src/main/java/com/example/cryptocoin/MainActivity.java
@@ -1,14 +1,58 @@
 package com.example.cryptocoin;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
-
+    BottomNavigationView bottomNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(navlistner);
+
+        if(savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_container, new dashboard())
+                    .commit();
+
+        }
     }
+    private BottomNavigationView.OnNavigationItemSelectedListener navlistner = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+            Fragment slectedFragment = null;
+            switch (item.getItemId()){
+                case R.id.home:
+                   // slectedFragment =new home();
+                    break;
+                case R.id.dashboard:
+                    slectedFragment =new dashboard();
+                    break;
+                case R.id.arrow:
+                   // slectedFragment =new arrow();
+                    break;
+                case R.id.price:
+                  //  slectedFragment =new price();
+                    break;
+                case R.id.config:
+                   // slectedFragment =new config();
+                    break;
+
+            }
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,slectedFragment).commit();
+            return true;
+        }
+    };
 }
